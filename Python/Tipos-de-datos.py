6741ac08d3d2c7c1e353d13f0b9ed5aadc0a166d
Palacios <josepaalaacioss@gmail.com>
2026-04-17 11:10:00 -0300

    Add data types examples in Tipos-de-datos.py

---
+++ Python/Tipos-de-datos.py
@@ -0,0 +1,15 @@
+#INT (INTEGER) --> Entero
+#String --> Cadena de caracteres
+#Float (flotante) --> Decimal
+#Bool (BOOLEANO) --> Operador logico
+
+#EJEMPLO DE USO:
+
+#1)
+
+numero = 5:
+numero2 = 6:
+suma = 5+6:
+
+#2)
+
