b31820c2f2cbbf7687c37c48500ff9a27730afa3
Palacios <josepaalaacioss@gmail.com>
2026-04-19 15:49:33 -0300

    Add variable examples for edad and nombre_completo

--- Python/01-Variables/ejemplos.py
+++ Python/01-Variables/ejemplos.py
@@ -1,1 +1,6 @@
+#Declaramos la variable
+edad = 15 #le asignamos un Tipo de dato que es el int (entero).
 
+#Otro ejemplo con un Tipo de dato string(cadena de caracteres).
+
+nombre_completo = "Lionel sanchez" #siempre en un string utilizar comillas --> " "
