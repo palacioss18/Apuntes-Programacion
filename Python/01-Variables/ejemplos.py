d8785d8c0a57a9bba4f81c354a30d821dd3fc85e
Palacios <josepaalaacioss@gmail.com>
2026-04-19 15:57:23 -0300

    Expand variable examples in ejemplos.py
    Added examples for string, boolean, and float data types, along with a sum operation.

--- Python/01-Variables/ejemplos.py
+++ Python/01-Variables/ejemplos.py
@@ -1,6 +1,18 @@
 #Declaramos la variable
 edad = 15 #le asignamos un Tipo de dato que es el int (entero).
 
-#Otro ejemplo con un Tipo de dato string(cadena de caracteres).
-
+#ejemplo con un Tipo de dato string(cadena de caracteres).
 nombre_completo = "Lionel sanchez" #siempre en un string utilizar comillas --> " "
+
+#Ejemplo con un Tipo de dato Bool(booleano).
+esta_lloviendo = True #True (verdadero) si esta lloviendo y False (falso) si no esta lloviendo,es un operador logico.
+
+#Ejemplo con un Tipo de dato Float(flotante).
+promedio = 7.45 #se utiliza para numeros flotantes (Decimales).
+
+#Tambien podemos hacer operaciones entre variables.
+#Ejemplo: sumar.
+a = 5
+b = 3
+#utilizamos una funcion para imprimir , mostrar en pantalla. que es el input.
+input(a+b) #Nos mostrara la suma de las variables
