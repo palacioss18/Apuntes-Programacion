99e59e852033e4dc2ff557bfb70f6001e58f6d82
Palacios <josepaalaacioss@gmail.com>
2026-04-24 23:49:50 -0300

    Add boolean variable declaration
    Added a boolean variable declaration to ejemplos.cpp.

--- c++/01-variables/ejemplos.cpp
+++ c++/01-variables/ejemplos.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 int main(){
+  //Variable
+  
   //Tipos de datos
   int num; //entero
   float num; //flotante
   char caracter; //un caracter
   double num; //igual que flotante solo que mayor capacidad de memoria
+  bool logico; //es true o false
+
+
 }
